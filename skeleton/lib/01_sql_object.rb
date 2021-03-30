f148c0bd6a545998d75fd2cda607d2c0f598b7ee
voldemort <alabenothman@gmail.com>
2021-03-30 14:36:06 -0700

    implemented methods

--- skeleton/lib/01_sql_object.rb
+++ skeleton/lib/01_sql_object.rb
@@ -2,21 +2,33 @@ require_relative 'db_connection'
 require 'active_support/inflector'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
-
+require 'byebug'
 class SQLObject
   def self.columns
-    # ...
+   array= DBConnection.execute2(<<-SQL)
+    SELECT 
+    *
+    FROM
+    #{table_name}
+
+    SQL
+    debugger
+    @columns = array.first.map do |ele| 
+      ele.to_sym
+    end
   end
 
   def self.finalize!
   end
-
+  
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
+    
   end
 
   def self.table_name
-    # ...
+    whatever= "#{self}".downcase 
+    @table_name = whatever.tableize
   end
 
   def self.all
